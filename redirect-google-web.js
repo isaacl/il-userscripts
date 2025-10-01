a345519927960bafd935fdc3789aeba06273f928
Isaac Levy <isaac.r.levy@gmail.com>
2025-10-01 15:11:36 -0400

    Add redirect-google-web
    Initial Commit

---
+++ redirect-google-web.js
@@ -0,0 +1,22 @@
+// ==UserScript==
+// @name         Always use Google Web mode (udm=14)
+// @namespace    https://github.com/isaacl
+// @version      0.1
+// @description  Redirects general Google searches to web mode (udm=14), avoiding some nonsense.
+// @author       Isaac Levy
+// @license      MIT
+// @match        *://www.google.com/search
+// @match        *://www.google.com/search?*
+// @run-at       document-start
+// ==/UserScript==
+
+(function() {
+    'use strict';
+    if (!window.location.search.includes('udm=14')) {
+        const urlParams = new URLSearchParams(window.location.search)
+        if (!urlParams.has('udm')) {
+          urlParams.append('udm', '14');
+          window.location.search = urlParams.toString();
+        }
+    }
+})();

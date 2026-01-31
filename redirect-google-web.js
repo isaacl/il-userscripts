11690365e3ccdd096e9d6fa03f0b6dcd82a0a96d
Isaac Levy <isaac.r.levy@gmail.com>
2026-01-30 21:12:57 -0500

    fix for userscripts app (needs IIFE)

--- redirect-google-web.js
+++ redirect-google-web.js
@@ -1,7 +1,7 @@
 // ==UserScript==
 // @name         Always use Google Web mode (udm=14)
 // @namespace    https://github.com/isaacl
-// @version      0.2.3
+// @version      0.2.4
 // @description  Redirects browser search to web mode (udm=14), avoiding AI and other distractions.
 // @author       Isaac Levy
 // @license      MIT
@@ -56,30 +56,29 @@
 // @supportURL   https://github.com/isaacl/il-userscripts/issues
 // ==/UserScript==
 
-"use strict";
+(function () {
+  "use strict";
+  // Note: udm param means a search engine mode, tbm is used for some legacy searches.
+  // Quick check to avoid unnecessary work.  Not for correctness.
+  const rawSearch = window.location.search;
+  if (rawSearch.includes("udm=") || rawSearch.includes("tbm=")) return;
 
-// Note: udm param means a search engine mode, tbm is used for some legacy searches.
-// Quick check to avoid unnecessary work.  Not for correctness.
-const rawSearch = window.location.search;
-if (rawSearch.includes("udm=") || rawSearch.includes("tbm="))
-  return;
-
-let fromGoogleSearch = false;
-if (document.referrer) {
-  try {
-    const { hostname, pathname } = new URL(document.referrer);
-    // Matches google.com, google.co.uk, google.com.au, etc.
-    const googleHost = /^(www\.)?google(\.[a-z]{2,3}){1,2}$/.test(hostname);
-    fromGoogleSearch = googleHost && pathname === "/search";
-  } catch {}
-}
-
-// Don't redirect if user may have explicitly clicked 'All' for a search.
-if (!fromGoogleSearch) {
-  const url = new URL(window.location.href);
-  // Don't set if already has a search choice param.
-  if (!url.searchParams.has("udm") && !url.searchParams.has("tbm")) {
-    url.searchParams.set("udm", "14");
-    window.location.replace(url);
+  let maybeFromGoogleSearch = false;
+  if (document.referrer) {
+    try {
+      const { hostname } = new URL(document.referrer);
+      // Matches google.com, google.co.uk, google.com.au, etc.
+      maybeFromGoogleSearch = /^(www\.)?google(\.[a-z]{2,3}){1,2}$/.test(hostname);
+    } catch {}
   }
-}
+
+  // Don't redirect if user may have explicitly clicked 'All' for a search.
+  if (!maybeFromGoogleSearch) {
+    const url = new URL(window.location.href);
+    // Don't set if already has a search choice param.
+    if (!url.searchParams.has("udm") && !url.searchParams.has("tbm")) {
+      url.searchParams.set("udm", "14");
+      window.location.replace(url.href);
+    }
+  }
+})();
